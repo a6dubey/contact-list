57d1dee665139daf6548aa167ef17703edf1836d
a6dubey <109572601+a6dubey@users.noreply.github.com>
2022-11-28 15:22:59 +0530

    Updates

---
+++ precache-manifest.e4d2dbb15108156855af2839610a4d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017b3aef35cd2d6f0c6f09f03bb65e31",
+    "url": "/contact-list/index.html"
+  },
+  {
+    "revision": "5cf33b466bd214f6f4ce",
+    "url": "/contact-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "03d96c5ebbe1633bde50",
+    "url": "/contact-list/static/js/2.5a770ac5.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/contact-list/static/js/2.5a770ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf33b466bd214f6f4ce",
+    "url": "/contact-list/static/js/main.30708926.chunk.js"
+  },
+  {
+    "revision": "91974ff1fcddc38d0689",
+    "url": "/contact-list/static/js/runtime-main.aabc9815.js"
+  }
+]);
